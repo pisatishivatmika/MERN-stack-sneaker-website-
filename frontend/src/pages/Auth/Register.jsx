ebb88e022b8e4855fdea0dd8b69162528246fc4e
pisatishivatmika <shivatmika.29@gmail.com>
2024-07-06 17:55:32 -0400

    initial commit

---
+++ frontend/src/pages/Auth/Register.jsx
@@ -0,0 +1,162 @@
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../../components/Loader";
+import { useRegisterMutation } from "../../redux/api/usersApiSlice";
+import { setCredentials } from "../../redux/features/auth/authSlice";
+import { toast } from "react-toastify";
+import { FaCentercode } from "react-icons/fa";
+
+const Register = () => {
+  const [username, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [register, { isLoading }] = useRegisterMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get("redirect") || "/";
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+    } else {
+      try {
+        const res = await register({ username, email, password }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        navigate(redirect);
+        toast.success("User successfully registered");
+      } catch (err) {
+        console.log(err);
+        toast.error(err.data.message);
+      }
+    }
+  };
+
+  return (
+    <section className="pl-[10rem] flex flex-wrap">
+      <div className="mr-[4rem] mt-[5rem]">
+        <h1 className="text-2xl font-semibold mb-4">REGISTER</h1>
+
+        <form onSubmit={submitHandler} className="container w-[40rem]">
+          <div className="my-[2rem]">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-white"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter name"
+              value={username}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+
+          <div className="my-[2rem]">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-white"
+            >
+              Email Address
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="my-[2rem]">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-white"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="my-[2rem]">
+            <label
+              htmlFor="confirmPassword"
+              className="block text-sm font-medium text-white"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Confirm password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+
+          <button
+            disabled={isLoading}
+            type="submit"
+            className="bg-green-600 text-white px-4 py-2 rounded cursor-pointer my-[1rem]"
+          >
+            {isLoading ? "Registering..." : "Register"}
+          </button>
+
+          {isLoading && <Loader />}
+        </form>
+
+        <div className="mt-4">
+          <p className="text-white">
+            Already have an account?{" "}
+            <Link
+              to={redirect ? `/login?redirect=${redirect}` : "/login"}
+              className="text-green-600 hover:underline"
+            >
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
+      
+<div className="flex items-center justify-center">
+  <div className="text-center">
+    <img 
+      src="https://lh3.googleusercontent.com/gg/AJIvXivmvb2TJlwqHL_SD4M0tj7PVRj2NNIDFOk5Kr2ehGorGcfS8NVkBPWBn5Jb1dXAIY5dYdUAsCkroF1ULQ4R4Ugvy8cQsGN1IbTZxvKTg-DlnjOHrcvX6P3wGf485y85fHrZW6BwakHXGlquRMOajEazj4YeSvZNiVF7ut0MKuCGr1FjLCSR6PeocCCHu3XIsJURk30OHIzGEZMdS6qSL45HVzalEFBZcMzxjY2nmBouU1vjsfGmJEVJkrqp-KAMkq7cwDePkN9-wDS6hGsLuVwXSYlnXNK5taLS7t87_3N3exB4SYtRjf66d-yMyRBaHZ-M34oEsMtAd3iDQs7Buc8R" 
+      alt="Login Image" 
+      className="mx-auto mb-4 w-200 h-200" 
+    />
+  </div>
+</div>
+    </section>
+  );
+};
+
+export default Register;
