d7ac7dae3a6c122d44003d19cd875e892d309e25
pisatishivatmika <shivatmika.29@gmail.com>
2025-02-03 16:37:47 -0500

    update2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MERS-stack-sneaker-website-/',
+  // base: '/MERS-stack-sneaker-website-/',
   server: {
     proxy: {
       "/api/": "http://localhost:8080",
