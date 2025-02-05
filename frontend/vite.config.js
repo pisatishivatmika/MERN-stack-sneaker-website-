4246b07860141307a2106be356e4b63d9970b3d6
pisatishivatmika <shivatmika.29@gmail.com>
2025-02-05 15:44:25 -0500

    changed base in vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/shivatmika-Ecommerce",
+  base: "/",
   server: {
     proxy: {
       "/api/": "https://shivatmika-ecommerce.onrender.com",
